5610a056a3feb58aca6ce65854998b21e28e5b1a
rahul123-dot <76402678+rahul123-dot@users.noreply.github.com>
2021-06-05 11:29:02 +0530

    Add files via upload

---
+++ Print_Hello_World.java
@@ -0,0 +1,10 @@
+package programs;
+
+public class Print_Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("Hello World!");
+	}
+
+}
